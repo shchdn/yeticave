fafc7c8f967f0ff00eaf8bf7d46cdefe34becae6
shchdn <shchedrin.georgy@gmail.com>
2020-10-16 01:24:12 +0300

    finished second task

--- index.php
+++ index.php
@@ -8,34 +8,39 @@ $categories = ['Доски и лыжи', 'Крепления', 'Ботинки',
 $products = [
     ['name' => '2014 Rossignol District Snowboard',
     'category' => 'Доски и лыжи',
-    'price' => '10999',
+    'price' => 10999,
     'image_path' => 'img/lot-1.jpg'],
 
     ['name' => 'DC Ply Mens 2016/2017 Snowboard',
     'category' => 'Доски и лыжи',
-    'price' => '159999',
+    'price' => 159999,
     'image_path' => 'img/lot-2.jpg'],
 
     ['name' => 'Крепление Union Contact Pro 2015 года размер L/XL',
     'category' => 'Крепления',
-    'price' => '8000',
+    'price' => 8000,
     'image_path' => 'img/lot-3.jpg'],
 
     ['name' => 'Ботинки для сноуборда DC Mutiny Charocal',
     'category' => 'Ботинки',
-    'price' => '10999',
+    'price' => 10999,
     'image_path' => 'img/lot-4.jpg'],
 
     ['name' => 'Куртка для сноуборда DC Mutiny Charocal',
     'category' => 'Одежда',
-    'price' => '7500',
+    'price' => 7500,
     'image_path' => 'img/lot-5.jpg'],
 
     ['name' => 'Маска Oakley Canopy',
     'category' => 'Разное',
-    'price' => '5400',
+    'price' => 5400,
     'image_path' => 'img/lot-6.jpg'],
 ];
+
+function get_price($value){
+    return number_format(ceil($value), 0, '.', ' ') . '<b class="rub">р</b>';
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -122,7 +127,7 @@ $products = [
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$item['price'] ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=get_price($item['price']) ?></span>
                         </div>
                         <div class="lot__timer timer">
 
