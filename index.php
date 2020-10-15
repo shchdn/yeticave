63c53fc1172ea58212b689a8514f75831a97005f
shchdn <shchedrin.georgy@gmail.com>
2020-10-16 00:59:35 +0300

    finished first task

--- index.php
+++ index.php
@@ -3,6 +3,39 @@ $is_auth = (bool) rand(0, 1);
 
 $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
+
+$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+$products = [
+    ['name' => '2014 Rossignol District Snowboard',
+    'category' => 'Доски и лыжи',
+    'price' => '10999',
+    'image_path' => 'img/lot-1.jpg'],
+
+    ['name' => 'DC Ply Mens 2016/2017 Snowboard',
+    'category' => 'Доски и лыжи',
+    'price' => '159999',
+    'image_path' => 'img/lot-2.jpg'],
+
+    ['name' => 'Крепление Union Contact Pro 2015 года размер L/XL',
+    'category' => 'Крепления',
+    'price' => '8000',
+    'image_path' => 'img/lot-3.jpg'],
+
+    ['name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+    'category' => 'Ботинки',
+    'price' => '10999',
+    'image_path' => 'img/lot-4.jpg'],
+
+    ['name' => 'Куртка для сноуборда DC Mutiny Charocal',
+    'category' => 'Одежда',
+    'price' => '7500',
+    'image_path' => 'img/lot-5.jpg'],
+
+    ['name' => 'Маска Oakley Canopy',
+    'category' => 'Разное',
+    'price' => '5400',
+    'image_path' => 'img/lot-6.jpg'],
+];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -44,8 +77,6 @@ $user_avatar = 'img/user.jpg';
                 </li>
             </ul>
             <?php endif; ?>
-        <!-- здесь должен быть PHP код для показа аватара пользователя -->
-
         </nav>
     </div>
 </header>
@@ -80,17 +111,18 @@ $user_avatar = 'img/user.jpg';
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
+            <?php foreach($products as $item) { ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src="<?= $item['image_path'] ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
+                    <span class="lot__category"><?=$item['category'] ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$item['name'] ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$item['price'] ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
 
@@ -98,6 +130,7 @@ $user_avatar = 'img/user.jpg';
                     </div>
                 </div>
             </li>
+            <?php } ?>
         </ul>
     </section>
 </main>
@@ -105,24 +138,9 @@ $user_avatar = 'img/user.jpg';
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <li class="nav__item">
-                <a href="all-lots.html">Доски и лыжи</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Крепления</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Ботинки</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Одежда</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Инструменты</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Разное</a>
-            </li>
+            <?php foreach($categories as $value) { ?>
+                <li class="nav__item"><a href="all-lots.html"><?=$value?></a></li>
+            <?php } ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
